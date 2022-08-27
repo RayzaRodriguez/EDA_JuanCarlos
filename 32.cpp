137d0854e443aaf5258322b0c58f5cec8aa7379c
RayzaRodriguez <rayza12_36@hotmail.com>
2022-08-26 23:52:55 -0500

    Add files via upload

---
+++ 32.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Vector
+{
+	int*m_pVect;
+	int m_nCount;
+	int m_nMax;
+	int m_nDelta=10;
+};
+
+void Resize(Vector *pThis)
+{
+	int *pTemp, i;
+	pTemp = new int[pThis->m_nMax + pThis->m_nDelta];
+	for(i = 0 ; i < pThis->m_nMax ; i++)
+		pTemp[i] = pThis->m_pVect[i];
+	delete [ ] pThis->m_pVect;
+	pThis->m_pVect = pTemp;
+	
+	pThis->m_nMax += pThis->m_nDelta;
+}
+
+void Insert(Vector *pThis, int elem)
+{
+	if( pThis->m_nCount == pThis->m_nMax ) 
+		Resize(pThis); 
+	pThis->m_pVect[pThis->m_nCount++] = elem;
+}
+
+
+	
+int main(int argc, char *argv[]) 
+{
+	Vector *fila=NULL;
+
+	for(int i=0; i<100; i++)
+	{
+		Insert(fila,i);
+	}
+	
+	
+	
+	return 0;
+}
+
---
+++ 33.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+
+
+class CVector
+{
+private:
+	int *m_pVect, 
+		m_nCount, 
+		m_nMax;
+	int m_nDelta=10; 
+	void Init(int delta); 
+	void Resize(); 
+public:
+	CVector(int delta)
+	{
+		m_nDelta = delta;
+	};
+	void Insert(int elem); 
+};
+
+void CVector:: Resize()
+{
+	const int delta = 10; 
+	int *pTemp, i;
+	pTemp = new int[m_nMax + m_nDelta]; 
+	for(i = 0 ; i < m_nMax ; i++) 
+		pTemp[i] = m_pVect[i]; 
+	delete [ ] m_pVect; 
+	m_pVect = pTemp; 
+	m_nMax += delta; 
+	
+	m_nMax += delta; 
+}
+
+void CVector::Insert(int elem)
+{
+	if( m_nCount == m_nMax ) 
+		Resize(); 
+	m_pVect[m_nCount++] = elem; 
+}
+
+
+
+int main(int argc, char *argv[]) 
+{
+	CVector fila(10);
+	for(int i=0; i<100; i++)
+	{
+		fila.Insert(i);
+	}
+	return 0;
+}
+
