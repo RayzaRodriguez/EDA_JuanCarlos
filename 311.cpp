b86707c6c2a122109f0f2b1c75013f9a3c35b13d
RayzaRodriguez <rayza12_36@hotmail.com>
2022-08-26 23:32:12 -0500

    Add files via upload

---
+++ 311.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int gVect[100];
+int gnCount;
+
+void Insert(int elem)
+{
+	if(gnCount < 100)
+		gVect[gnCount++] = elem;
+}
+
+int main(int argc, char *argv[]) 
+{
+	for(int i=0; i<100; i++)
+	{
+		Insert(i);
+	}
+	for(int i=0; i<100; i++)
+	{
+		cout<<gVect[i]<<" ";
+	}
+	
+	return 0;
+}
+
---
+++ 312.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int *gpVect = NULL; 
+int gnCount = 0;
+int gnMax = 0;
+
+void Resize()
+{
+	const int delta = 10; 
+	//gpVect = realloc(gpVect, sizeof(int) * (gnMax + delta));
+	int *pTemp, i;
+	pTemp = new int[gnMax + delta];
+	for(i = 0 ; i < gnMax ; i++)
+	pTemp[i] = gpVect[i];
+	delete [ ] gpVect;
+	gpVect = pTemp;
+	
+	gnMax += delta; 
+}
+
+void Insert(int elem)
+{
+	if( gnCount == gnMax ) 
+		Resize(); 
+	gpVect[gnCount++] = elem; 
+}
+
+int main(int argc, char *argv[]) 
+{
+	for(int i=0; i<100; i++)
+	{
+		Insert(i);
+	}
+	for(int i=0; i<100; i++)
+	{
+		cout<<gpVect[i]<<" ";
+	}
+	return 0;
+}
+
